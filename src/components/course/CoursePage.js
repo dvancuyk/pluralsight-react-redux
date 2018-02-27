93f7220066478037241a56e4a900a4b8f00d98aa
Derek Van Cuyk <dvancuyk@gmail.com>
2018-02-27 08:41:16 -0500

    Added course page

---
+++ src/components/course/CoursePage.js
@@ -0,0 +1,16 @@
+import React, {PropTypes } from 'react';
+
+class CoursesPage extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+      </div>
+    );
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
 import AboutPage from './components/about/AboutPage';
+import CoursePage from './components/course/CoursePage';
 import HomePage from './components/home/HomePage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
+    <Route path="course" component={CoursePage} />
     <Route path="about" component={AboutPage} />
   </Route>
 );
